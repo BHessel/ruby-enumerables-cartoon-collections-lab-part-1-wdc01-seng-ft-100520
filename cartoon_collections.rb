8a7839a8c19c06bc5d4537a3d503c5e40194d6ad
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-16 20:45:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,6 @@ end
 def list_dwarves(array)
   array.each do |numname|
     puts |numname|
-end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
